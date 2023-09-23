97afc51e37c49718dc4831c93ec53518ebc798d5
VeiledBit <12989770+VeiledBit@users.noreply.github.com>
2023-09-23 06:10:16 +0200

    Add filtering by categories

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable no-unused-vars */
 import { useEffect, useState } from "react";
 import axios from "axios";
 import SaleItem from "../../components/SaleItem";
@@ -6,7 +7,13 @@ import InputLabel from "@mui/material/InputLabel";
 import MenuItem from "@mui/material/MenuItem";
 import FormControl from "@mui/material/FormControl";
 import Select from "@mui/material/Select";
-import { TextField } from "@mui/material";
+import {
+  Checkbox,
+  Divider,
+  FormControlLabel,
+  FormGroup,
+  TextField,
+} from "@mui/material";
 import baseUrl from "../../config/url";
 
 export default function Home() {
@@ -14,27 +21,47 @@ export default function Home() {
   const [store, setStore] = useState("maxi");
   const [search, setSearch] = useState("");
   const [sort, setSort] = useState("category");
+  const [categoryValues, setCategoryValues] = useState([]);
+  const [selectAllCategories, setSelectAllCategories] = useState(true);
+  const [categories, setCategories] = useState(categoryValues);
   let delayTimer;
+
+  useEffect(() => {
+    axios
+      .get(`${baseUrl}/saleItems/${store}/categories`)
+      .then((response) => {
+        setCategoryValues(response.data);
+        setCategories(response.data);
+      })
+      .catch();
+  }, [store]);
 
   useEffect(() => {
     if (search === "" || search.length < 2) {
       axios
-        .get(`${baseUrl}/saleItems/${store}/?sort=${sort}`)
+        .get(
+          `${baseUrl}/saleItems/${store}/?sort=${sort}&categories=${categories.join(
+            "|"
+          )}`
+        )
         .then((response) => {
           setSaleItems(response.data);
-          console.log(response.data);
         })
         .catch();
     } else {
       axios
-        .get(`${baseUrl}/saleItems/${store}/?search=${search}&sort=${sort}`)
+        .get(
+          `${baseUrl}/saleItems/${store}/?search=${search}&sort=${sort}&categories=${categories.join(
+            "|"
+          )}`
+        )
         .then((response) => {
           setSaleItems(response.data);
           console.log(response.data);
         })
         .catch();
     }
-  }, [store, search, sort]);
+  }, [store, search, sort, categories]);
 
   const handleChange = (event) => {
     setStore(event.target.value);
@@ -47,9 +74,32 @@ export default function Home() {
   const handleChangeSearchItems = (event) => {
     clearTimeout(delayTimer);
     delayTimer = setTimeout(() => {
-      setSearch(event.target.value)
+      setSearch(event.target.value);
     }, 750);
   };
+
+  const handleChangeCategories = (event) => {
+    const {
+      target: { value },
+    } = event;
+    if (value.length === categoryValues.length && !selectAllCategories) {
+      setSelectAllCategories(true);
+    } else {
+      setSelectAllCategories(false);
+    }
+    setCategories(typeof value === "string" ? value.split(",") : value);
+  };
+
+  const handleChangeAllCategories = (event) => {
+    const checked = event.target.checked;
+    setSelectAllCategories(checked);
+    if (checked) {
+      setCategories(categoryValues);
+    } else {
+      setCategories([]);
+    }
+  };
+
   return (
     <>
       <div className={styles.gridSettings}>
@@ -83,6 +133,38 @@ export default function Home() {
             <MenuItem value="category">Kategorija</MenuItem>
             <MenuItem value="discountHighest">Najveci popust</MenuItem>
             <MenuItem value="discountLowest">Najnizi popust</MenuItem>
+          </Select>
+        </FormControl>
+        <FormControl className={styles.selectCategoryForm} fullWidth>
+          <InputLabel id="selectCategory">Kategorije</InputLabel>
+          <Select
+            multiple
+            className={styles.selectCategory}
+            value={categories}
+            label="selectCategory"
+            onChange={handleChangeCategories}
+            renderValue={(selected) => selected.join(", ")}
+          >
+            <FormGroup>
+              <FormControlLabel
+                control={
+                  <MenuItem>
+                    <Checkbox
+                      checked={selectAllCategories}
+                      onChange={handleChangeAllCategories}
+                    />
+                  </MenuItem>
+                }
+                label="Sve"
+              />
+            </FormGroup>
+            <Divider />
+            {categoryValues.map((item) => (
+              <MenuItem key={item} value={item}>
+                <Checkbox checked={categories.indexOf(item) > -1} />
+                {item}
+              </MenuItem>
+            ))}
           </Select>
         </FormControl>
       </div>
