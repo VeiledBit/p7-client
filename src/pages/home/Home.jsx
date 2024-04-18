2f79d2bbd978969b7e503a91f9c1482bd5080822
VeiledBit <12989770+VeiledBit@users.noreply.github.com>
2024-04-18 16:07:44 +0200

    Refresh data when session changes

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -140,7 +140,7 @@ export default function Home() {
     };
 
     setUpSession();
-  }, [isSearchAllStoresChecked, store, search, sort, categories]);
+  }, [isSearchAllStoresChecked, store, search, sort, categories, session]);
 
   const handleChange = (event) => {
     setStore(event.target.value);
